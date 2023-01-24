ec978ac2fdf24d2ab02d64f5cef2fb2420d1b719
mathewh-vhc <120552858+mathewh-vhc@users.noreply.github.com>
2023-01-24 10:29:05 -0600

    changes css

--- src/Ticket/TicketInfoContainer.jsx
+++ src/Ticket/TicketInfoContainer.jsx
@@ -59,16 +59,40 @@ class WorkOrderInfo extends Component {
     
     render() {
         return(
-            <div className = "container">
-                <div className = "wo-info-prop">
-                    {this.props.wo.id}
+            <div className = "container" id = "wo-info-cont">
+                <div className = "wo-info-prop">{this.props.wo.id}</div>
+                <div className = "wo-info-prop">{this.props.wo.custcode}</div>
+
+                <div class="container" id="wo-contact-cont">
+                    <div className = "wo-info-prop">{this.props.wo.contactname}</div>
+                    <div className = "wo-info-prop">{this.props.wo.contactphone}</div>
+                    <input value={this.props.wo.contactemail}></input>
+                    <div className = "wo-info-prop">{this.props.wo.street}</div>
+                    <div className = "wo-info-prop">{this.props.wo.unit}</div>
+                    <div className = "wo-info-prop">{this.props.wo.cityzip}</div>
+                    <div className = "wo-info-prop">{this.props.wo.state}</div>
                 </div>
-                <div className = "wo-info-prop">
-                    {this.props.wo.contactname}
+                <div class = "container" id="wo-reference-cont">
+                    <div>Sales Rep:</div><div className = "wo-info-prop">{this.props.wo.salesrep}</div>
+                    <div>Taken By:</div><div className = "wo-info-prop">{this.props.wo.takenby}</div>
+                    <div>Depart:</div><div className = "wo-info-prop">{this.props.wo.dept}</div>
+                    <div>Category:</div><div className = "wo-info-prop">{this.props.wo.cat}</div>
+                    <div>Reference:</div><div className = "wo-info-prop">{this.props.wo.ref}</div>
+                    <div>Status:</div><div className = "wo-info-prop">{this.props.wo.status}</div>
+                    <div>Job Ref.:</div><div className = "wo-info-prop">{this.props.wo.jobref}</div>
+                    <div>Contract:</div><div className = "wo-info-prop">{this.props.wo.conref}</div>
                 </div>
-                <div className = "wo-info-prop">
-                    {this.props.wo.contactemail}
+                <div class = "container" id="wo-pricing-cont">
+                    <div>Price Book:</div><div className = "wo-info-prop">{this.props.wo.pricebook}</div>
+                    <div>Price Level:</div>
+                    <select value = {this.props.wo.pricelevel}>
+                        <option value='STA'>STA</option>
+                        <option value='CLA'>CLA</option>
+                        <option value='PRE'>PRE</option>
+                        <option value='ULT'>ULT</option>
+                    </select>
                 </div>
+                <textarea>{this.props.wo.descr}</textarea>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './style/vhp-react-app.css';
 import './style/vhp-react-toolbar.css';
 import './style/MenuBar.css';
+import './style/TicketInfo.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
