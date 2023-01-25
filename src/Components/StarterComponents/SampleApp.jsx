3d32eb2ca626c5a3c2487676073f58aaf864b556
mathewh-vhc <120552858+mathewh-vhc@users.noreply.github.com>
2023-01-25 16:17:14 -0600

    Added sample app component

--- src/Components/StarterComponents/SampleApp.jsx
+++ src/Components/StarterComponents/SampleApp.jsx
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import { MenuTabBar } from './Components/VHP/ViewControllers/MenuTabBar';
 
 /**
- * Starter class for a main container. This sits below the title bar and tracks the current view
- * and all open containers
- * Containers are dynamically generated from props.
+ * Starter class for an application. Contains a set of containers with a MenuBar allowing you to swap between tabs.
+ * MenuBar style can be customized, and containers can be added to the Container to add pages to your app
+ * Clone and modify this file whenever you want to create a new app that can sit inside App.js or the dock.
  */
-export class SpiffApp extends Component {
+export class SampleApp extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
             active: true,
             Containers:[
-                <div style={{top:"180px", position:"absolute"}}>Open</div>,
-                <div style={{top:"180px", position:"absolute"}}>Active</div>,
-                <div style={{top:"180px", position:"absolute"}}>Closed</div>
+                <div style={{top:"180px", position:"absolute"}}>Page 1</div>,
+                <div style={{top:"180px", position:"absolute"}}>Page 2</div>,
+                <div style={{top:"180px", position:"absolute"}}>Page 3</div>
             ],
             currentTab:0
         }
@@ -38,7 +38,7 @@ export class SpiffApp extends Component {
         return(
             <>
                 <MenuTabBar 
-                    tabs = {["Open", "Active", "Closed"]}
+                    tabs = {["Page 1", "Page 2", "Page 3"]}
                     SetTab = {this.SetTab}
                     MenuStyle = "top-bar"
                 />
